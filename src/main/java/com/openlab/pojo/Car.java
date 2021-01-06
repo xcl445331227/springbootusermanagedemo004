7d6717a8d69d1fb22d303ae0af80621770fd4921
xcl <445331227@qq.com>
2021-01-06 17:37:07 +0800

    增加Car类

---
+++ src/main/java/com/openlab/pojo/Car.java
@@ -0,0 +1,25 @@
+package com.openlab.pojo;
+
+public class Car {
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+}
